55b704df438ca7e13fc698c37402fc4b8f1cbbb1
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-12-29 22:09:46 +0530

    method reference and constructor reference added

--- Streams/JavaFunctionalInterfaces/JavaStreamPractice.java
+++ Streams/JavaFunctionalInterfaces/JavaStreamPractice.java
@@ -1,10 +1,18 @@
 
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 class JavaStreamPractice{
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
 
@@ -50,5 +58,62 @@ print.accept("Hello World");
 
 Supplier<String> givemeHelloworld = ()->"Hello World from Supplier";
 System.out.println(givemeHelloworld.get());
+
+Function<Integer,Integer> multiply = x->x*10;
+Predicate<Integer>greaterThan = x->x>50;
+Consumer<Integer> printIt = x->System.out.println(x);
+Supplier<Integer> giveMeNumber = ()-> 60;
+
+
+if(greaterThan.test(giveMeNumber.get())){
+    printIt.accept(multiply.apply(giveMeNumber.get()));
+ }
+
+
+ //Bifunction ,BiConsumer , BiPredicate ,BiSupplier
+
+
+ BiPredicate <Integer,Integer> isSumEven = (x,y)-> (x+y)%2==0;
+System.out.println(isSumEven.test(4,6));
+
+
+BiConsumer<String,Integer> biConsumer = (s,num)-> System.out.println(s+" "+num);
+biConsumer.accept("Shubham",90);
+
+BiFunction<String,String,Integer> biFunction = (x,y)-> (x+y).length();
+System.out.println(biFunction.apply("Hello ","World"));
+
+
+//Function<Integer,Integer> function = x-> x*x;
+UnaryOperator<Integer> unaryOperator = x-> x+x;
+BinaryOperator<Integer>binaryOperator= (x,y)-> x*y;
+System.out.println(unaryOperator.apply(5));
+System.out.println(binaryOperator.apply(5,6));
+ 
+//Method Refererence -> Tommorow
+
+
+
+ List<String> students = Arrays.asList("Ram","Shyam","Mohan");
+
+ students.forEach(x->system.out.println(x));
+
+ //This is Method Reference
+ students.forEach(System.out::println);
+
+ 
+
+ //Constructor Reference
+ List<Student> std = students.stream().map(Student::new).collect(Collectors.toList());
+
+
+
+}
+}
+
+class Student{
+    String name;
+    Student(String name){
+        this.name=name;
     }
 }
